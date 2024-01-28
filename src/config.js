dbe0bc5d6314b702815e343990d43930cace6367
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-01-28 15:44:34 -0600

    Update default values in config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-export const JWT_KEY = process.env.JWT_KEY
-export const PORT = process.env.PORT
-export const MONGO_USER = process.env.MONGO_USER
-export const MONGO_PASS = process.env.MONGO_PASS
+export const JWT_KEY = process.env.JWT_KEY || 'secret'
+export const PORT = process.env.PORT || 3000
+export const MONGO_USER = process.env.MONGO_USER || 'admin'
+export const MONGO_PASS = process.env.MONGO_PASS || 'admin'
