6f9bd962593a4158e58da6c83264fa5b16d47001
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-01-28 19:31:53 -0600

    Testing Db Connection

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
         const conn = await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.7lcidco.mongodb.net/?retryWrites=true&w=majority`)
         Csuccess(`Connected at ${conn.connection.host}`, nMod.db);
     } catch (error) {
-        Cerror(error.message, nMod.db);
+        Cerror(error, nMod.db);
         Cinfo(`User: ${process.env.MONGO_USER} | Pass: ${process.env.MONGO_PASS}`, nMod.db);
         process.exit(1);
     }
