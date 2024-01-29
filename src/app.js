3b1957df07b60660165781bcd06a207e6b4708e3
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-01-29 11:03:07 -0600

    Add cors package

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import Express from "express";
 import morgan from "morgan";
 import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
-import Cors from "./middlewares/Cors.js";
+import Cors from "cors";
 
 const app = Express();
 
 app.use(morgan("dev"));
 app.use(Express.json());
 app.use(cookieParser());
-app.use(Cors);
+app.use(Cors({ credentials: true }));
 app.use('/api', routes);
 
 export default app;
--- src/middlewares/Cors.js
+++
@@ -1,7 +0,0 @@
-const allowedOrigins = "*"
-
-export default function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", allowedOrigins);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-}
