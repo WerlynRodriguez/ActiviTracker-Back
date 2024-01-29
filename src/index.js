6260e2153bf6b9debaed0a032334f1e98be7a2ce
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-01-28 19:23:15 -0600

    Testing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import app from "./app.js";
 import jobs from "./jobs/index.js";
 import { connectDB } from "./db.js";
 import { Cinfo, nMod } from "./libs/console.js";
-import 'dotenv/config'
+import 'dotenv/config.js'
 
 app.listen(process.env.PORT, async () => {
     await connectDB();
     await jobs();
     
     Cinfo(`Server running on port ${process.env.PORT}`, nMod.app);
+    Cinfo(`Jwt`, process.env.JWT_KEY);
 });
